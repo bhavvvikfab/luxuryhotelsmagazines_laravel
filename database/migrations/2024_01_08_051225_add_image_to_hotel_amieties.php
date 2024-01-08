ac389bd73b512c78049a856d95fe31545a72fc0b
bhavvvikfab <121284713+bhavvvikfab@users.noreply.github.com>
2024-01-08 14:36:42 +0530

    Update 2024_01_08_051225_add_image_to_hotel_amieties.php

--- database/migrations/2024_01_08_051225_add_image_to_hotel_amieties.php
+++ database/migrations/2024_01_08_051225_add_image_to_hotel_amieties.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('hotel_amieties', function (Blueprint $table) {
-            $table->text('imag')->after('type');
+            $table->text('image')->after('type');
             //
         });
     }
