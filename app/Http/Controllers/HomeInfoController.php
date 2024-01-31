94085b9cb582c2c27ccbf52831bd74c71cf8724e
akshay <akshayfablead@gmail.com>
2024-01-31 15:00:27 +0530

    commit branch_akshay_023

--- app/Http/Controllers/HomeInfoController.php
+++ app/Http/Controllers/HomeInfoController.php
@@ -242,6 +242,141 @@ else if ($hotel_type == 4)
                     }
 
 }
+else if ($hotel_type == 5) 
+{
+
+$rules = [
+    'title' => 'required',
+    'image' => 'required|image', // Add appropriate validation for image uploads
+    'country' => 'required',
+    'you_tube_link' => 'required',
+    'content' => 'required',
+    'background_title' => 'required',
+    'background_sub_title' => 'required',
+
+];
+
+$requestData = $request->all();
+$validator = Validator::make($requestData, $rules);
+
+if ($validator->fails()) {
+    $response['status'] = false;
+    $response['message'] = $validator->messages();
+} else {
+
+    $homeInfo = HomeInfoModel::where('type', $requestData['type'])->first();
+    $detail = $homeInfo['details'];
+    $bg_data = json_decode($detail);
+
+        
+    
+
+    $digitalCampaignDetails = [
+        'title' => $requestData['title'],
+        'image' => $request->file('image')->store('uploads'), // Adjust the storage path as needed
+        'country' => $requestData['country'],
+        'you_tube_link' => $requestData['you_tube_link'],
+        'content' => $requestData['content'],
+    ];
+
+   
+    if (!$homeInfo) {
+        // $home_info->details = json_encode([$homeaboutDetails]);
+        $homeInfo->details = json_encode([$homeaboutDetails],['sections' => [$digitalCampaignDetails]]);
+    } else {
+        $detailsArray = json_decode($homeInfo->details, true);
+        //  $detailsArray = $homeaboutDetails;
+        $detailsArray['sections'][] = $digitalCampaignDetails;
+
+        // $homeInfo = new HomeInfoModel();
+        $homeInfo->type = $requestData['type'];
+
+        $detailsArray['background_title'] =  $requestData['background_title'];
+        $detailsArray['background_sub_title'] = $requestData['background_sub_title'];
+        if ($request->hasFile('background_image') && $request->file('background_image')->isValid()) {
+            $detailsArray['background_image'] = $request->file('background_image')->store('uploads');
+        }
+        $homeInfo->details = json_encode($detailsArray);
+    }
+
+    $homeInfo->save();
+
+    if ($homeInfo) {
+                                        $response = response()->json(['status' => true, 'message' => 'Home Info Updated Successfully']);
+                                    } else {
+                                        $response = response()->json(['status' => false, 'message' => 'Failed to add home info!']);
+                                    }
+        
+    // if ($homeInfo) {
+    //     $response['status'] = true;
+    //     $response['message'] = 'Home Info Updated Successfully';
+    // } else {
+    //     $response['status'] = false;
+    //     $response['message'] = 'Failed to add home info!';
+    // }
+}
+}
+
+else if ($hotel_type == 6) 
+{
+
+    $rules = [
+        'type' => 'required',
+        'title' => 'required',
+        'hotel_type' => 'required',
+        'category' => 'required',
+        'pos' => 'required',
+    ];
+
+    $validator = Validator::make($request->all(), $rules);
+
+    if ($validator->fails()) {
+        $response['message'] = $validator->messages();
+    } else {
+        
+        // if ($hotel_type == 1) {
+            
+
+            $headerSliderDetails = [
+                'title' => $request->input('title'),
+                'hotel_type' => $request->input('hotel_type'),
+                // 'category' => $request->file('category')->store('uploads'),
+                'pos' => $request->input('pos'),
+            ];
+
+            if ($request->hasFile('category')) {
+                $headerSliderDetails['category'] = $request->file('category')->store('uploads');
+            }
+            
+
+            $home_info = HomeInfoModel::where('type', $request->input('type'))->first();
+            // dd($home_info);
+
+
+            if (!$home_info) {
+                $home_info = new HomeInfoModel();
+                $home_info->type = $request->input('type');
+                $home_info->details = json_encode([$headerSliderDetails]);
+            } else {
+                $detailsArray = json_decode($home_info->details, true);
+                $detailsArray[] = $headerSliderDetails;
+                $home_info->details = json_encode($detailsArray);
+            }
+
+            $home_info->save();
+
+            if ($home_info) {
+                $response = response()->json(['status' => true, 'message' => 'Home Info Added Successfully']);
+            } else {
+                $response = response()->json(['status' => false, 'message' => 'Failed to add home info!']);
+            }
+        // }
+    }
+}
+
+
+// return response()->json($response);
+
 
     return $response;
     
@@ -325,8 +460,83 @@ public function edit_home_info(Request $request)
          
             }
 
+            // if($home_info['type']==5){
+            //      $home_info_detail = json_decode($home_info['details'], true);
+            // //    dd($home_info_detail);
+            //   $details['background_image'] =  asset("storage/app/".$home_info_detail['background_image']);
+            // // $home_info['details'] = $home_info_detail;  
 
+            // $details = [];
+            // foreach ($home_info_detail['sections'] as $detail) {
+            //     // dd($detail);
+            //     $details[] = [
+            //         'image' => asset('storage/app/'.$detail['image']),
+            //          'title' => $detail['title'],
+            //          'country' => $detail['country'],
+            //          'you_tube_link'=> $detail['you_tube_link'],
+            //          'content'=> $detail['content'],
+            //     ];
+            // }
             
+            // $home_info['details'] = $details;
+           
+
+            // }
+
+            if ($home_info['type'] == 5) {
+                $home_info_detail = json_decode($home_info['details'], true);
+            
+                // Create an array to store details
+                $details = [];
+            
+                // Extract background_image from $home_info_detail
+                $background_image = asset("storage/app/" . $home_info_detail['background_image']);
+                $background_title = $home_info_detail['background_title'];
+                $background_sub_title = $home_info_detail['background_sub_title'];
+            
+                // Loop through sections and populate $details array
+                foreach ($home_info_detail['sections'] as $detail) {
+                    $details[] = [
+                        'image' => asset('storage/app/' . $detail['image']),
+                        'title' => $detail['title'],
+                        'country' => $detail['country'],
+                        'you_tube_link' => $detail['you_tube_link'],
+                        'content' => $detail['content'],
+                    ];
+                }
+            
+                // Add background_image to $details array
+                $home_info_detail['background_image'] = $background_image;
+                $home_info_detail['background_title'] = $background_title;
+                $home_info_detail['background_sub_title'] = $background_sub_title;
+                $home_info_detail['sections'] = array_values($details);
+            
+                // Update $home_info['details'] with the modified $details array
+                $home_info['details'] = $home_info_detail;
+            }
+            
+
+
+            if($home_info['type']==6){
+                // $home_info_detail = json_decode($home_info['details'], true);
+            // dd($home_info_detail);
+
+            $details = [];
+            foreach ($home_info_detail as $detail) {
+                // dd($detail);
+                $details[] = [
+                    'category' => asset('storage/app/'.$detail['category']),
+                     'title' => $detail['title'],
+                     'pos' => $detail['pos'],
+                     'hotel_type'=> $detail['hotel_type'],
+                ];
+            }
+            
+            $home_info['details'] = $details;
+           
+
+            }
+
             
             $data = $home_info;
             $response = response()->json(['status' => true, 'message' => 'Home Info Data Found', 'data' => $data]);
@@ -367,17 +577,25 @@ public function delete_home_info(Request $request)
     } else {
 
     $home_info_type = $requestData['type'];
+    
     $key = $requestData['key'];
 
 
     $home_info = HomeInfoModel::where('type',$home_info_type)->first();
+    // dd($home_info['type']);
+    
 
 
         if (!$home_info) {
-            return response()->json(['message' => 'Hotel Info not found'], 404);
+            return response()->json(['message' => 'Home Info not found'], 404);
         }
+        if ($home_info['type'] == 3 || $home_info['type'] == 6 || $home_info['type'] == 1) 
+        {
+           
 
         $home_detail = json_decode($home_info['details']);
+        // dd($home_detail);
+
         if (array_key_exists($key, $home_detail)) {
             // Remove the file using the provided key
             unset($home_detail[$key]);
@@ -395,7 +613,111 @@ public function delete_home_info(Request $request)
         }
     }
 
+    if ($home_info['type'] == 5) {
+        $home_detail = json_decode($home_info['details'], true);
+    
+        // Get all sections data
+        $sections = $home_detail['sections'];
+    
+        if (array_key_exists($key, $sections)) {
+            // Remove the section using the provided key
+            unset($sections[$key]);
+    
+            // Reindex array keys
+            $sections = array_values($sections);
+    
+            // Update detailsArray with the modified sections
+            $detailsArray = json_decode($home_info->details, true);
+            $detailsArray['sections'] = $sections;
+    
+            $home_info->details = json_encode($detailsArray);
+            $home_info->save();
+    
+            $response['status'] = true;
+            $response['message'] = 'Home Info deleted successfully.';
+        } else {
+            $response['message'] = 'Invalid key provided.';
+        }
+    }
+
+}
 return response()->json($response);
+
+
+//     else if($home_info['type'] == 5)
+//     {
+//         // echo "trhrth";
+//         $home_detail = json_decode($home_info['details'], true);
+
+// // Get all sections data
+// $sections = $home_detail['sections'];
+// // dd($sections);
+
+
+
+// if (array_key_exists($key, $sections)) {
+//     // Remove the file using the provided key
+//     unset($sections[$key]);
+//     // Reindex array keys
+//     $section = array_values($sections);
+//     // dd($section);
+
+//     $detailsArray = json_decode($section->details, true);
+//     $detailsArray['sections'][] = $home_detail;
+//     $homeInfo->details = json_encode($detailsArray);
+
+
+//     // Update the 'file_pdf' array in the database
+//     // $home_info->details = json_encode($sections);
+//     $home_info->save();
+
+//     $response['status'] = true;
+//     $response['message'] = 'Home Info deleted successfully.';
+// } else {
+//     $response['message'] = 'Invalid key provided.';
+// }
+
+
+
+// // foreach ($sections as $index => $section) {
+// //     // dd($section);
+// //     // Access data from each section
+// //     $title = $section['title'];
+// //     $image = $section['image'];
+// //     $country = $section['country'];
+// //     $you_tube_link = $section['you_tube_link'];
+// //     $content = $section['content'];
+
+// //     // Process or use the data as needed
+
+// //     // Unset the current index
+// //     unset($home_detail['sections'][$index]);
+// //     // unset($sections[$index]);
+// // }
+// // $home_detail['sections'] = array_values($home_detail['sections']);
+
+// // // $sections = array_values($sections);
+// // dd($home_detail['sections']);
+
+// // Optionally, update the original array with modified sections
+// // $home_detail['sections'] = $sections;
+
+// // If you want to encode it back to JSON
+// // $updated_details_json = json_encode($home_detail);
+
+// // Print or use the updated JSON
+// // dd($updated_details_json);
+
+
+
+
+//         // $response['status'] = true;
+
+//     }
+
+//     }
+
+
 }
 
     }
