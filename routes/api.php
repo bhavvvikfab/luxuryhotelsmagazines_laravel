aa3ff661ec2d32b33a9f50cc4be3305a14cf6017
akshay <akshayfablead@gmail.com>
2024-02-12 12:23:40 +0530

    commit branch_akshay_030

--- routes/api.php
+++ routes/api.php
@@ -87,9 +87,81 @@ Route::middleware(['Api_Auth'])->group(function () {
     
     
     Route::post('/search-hotel', [HotelController::class, 'SearchHotel']);
+
+    Route::get('/all-hotels', [HotelController::class, 'AllHotels']);
+    Route::post('/edit-hotels', [HotelController::class, 'EditHotels']);
+
+    Route::get('/all-distibutor', [DistibutionController::class, 'AllDistributor']);
+    Route::post('/edit-distibutor', [DistibutionController::class, 'EditDistributor']);
+
+
+    Route::get('/all-distibutor-data', [DistibutionController::class, 'AllDistributorData']);
+    Route::post('/edit-distibutor-data', [DistibutionController::class, 'EditDistributorData']);
+
+    Route::get('/all-distibutor-detail', [DistibutionController::class, 'AllDistributorDetail']);
+    Route::post('/edit-distibutor-detail', [DistibutionController::class, 'EditDistributorDetail']);
+
+    Route::get('/all-news/{id?}', [NewsController::class, 'index']);
+    Route::post('/edit-news', [NewsController::class, 'EditNews']);
+    Route::post('/views-news', [NewsController::class, 'ViewNews']);
+
+    Route::post('/edit_news_category', [NewsController::class, 'edit_news_category']);
+    Route::get('/all_news_category', [NewsController::class, 'all_news_category']);
+
+    Route::get('/all-home-hotel', [HomeHotelController::class, 'AllHomeHotel']);
+    Route::post('/edit-home-hotel', [HomeHotelController::class, 'EditHomeHotel']);
+    Route::post('/limit-all-home-hotel', [HomeHotelController::class, 'LimitAllHomeHotel']);
+
+    Route::post('/edit-hotel-magazines', [HotelMagazinesController::class, 'EditHotelMagazines']);
+
+    Route::get('/all-hotel-ameties', [HotelController::class, 'AllHotelAmeties']);
+    Route::post('/edit-hotel-ameties', [HotelController::class, 'EditHotelAmeties']);
+
+    Route::get('/all-hotel-facilities', [HotelFacilitiesController::class, 'AllHotelFacilities']);
+    Route::post('/edit-hotel-facilities', [HotelFacilitiesController::class, 'EditHotelFacilities']);
+
+    Route::get('/all-media-kit', [MediakitController::class, 'AllMediaKit']);
+    Route::post('/edit-media-kit', [MediakitController::class, 'EditMediaKit']);
+
+    Route::get('/all-team', [TeamController::class, 'AllTeam']);
+    Route::post('/edit-team', [TeamController::class, 'EditTeam']);
+
+
+    Route::get('/all-banner', [BannerController::class, 'AllBanner']);
+    Route::post('/edit-banner', [BannerController::class, 'EditBanner']);
+
+    Route::get('/all-query', [QueriesController::class, 'AllQuery']);
+    Route::post('/edit-query', [QueriesController::class, 'EditQuery']);
+
+    Route::get('/all-voted-hotel', [VotedHotelController::class, 'AllVotedHotel']);
+    Route::post('/edit-voted-hotel', [VotedHotelController::class, 'EditVotedHotel']);
+
+    Route::get('/all-hotel-properties', [PropertiesController::class, 'AllHotelProperties']);
+    Route::post('/edit-hotel-properties', [PropertiesController::class, 'EditHotelProperties']);
+
+    Route::get('/all-package-price', [PackagePriceController::class, 'AllPackagePrice']);
+    Route::post('/edit-package-price', [PackagePriceController::class, 'EditPackagePrice']);
+
+    Route::get('/all-subscriber', [SubscribersController::class, 'AllSubscriber']);
+    Route::post('/edit-subscriber', [SubscribersController::class, 'EditSubscriber']);
+
+    Route::get('/edit-about-us', [About_Us_Controller::class, 'index']);
+    Route::post('/edit-magzine-distributed', [About_Us_Controller::class, 'edit_magzine_distributed']);
+
+    Route::get('/all-review/{type?}', [ReviewController::class, 'index']);
+
+    Route::post('/edit_home_info', [HomeInfoController::class, 'edit_home_info']);
+
+    Route::post('/get_single_page_details', [HomeInfoController::class, 'get_single_page_details']);
+    Route::get('/all-review-categories', [HotelController::class, 'All_Review_Categories']);
+
+    Route::post('/edit_magazine_cover', [HomeInfoController::class, 'edit_magazine_cover']);
+    Route::get('/all-review-topics', [HotelController::class, 'All_Review_Topics']);
     
     // Route::get('/about-us', [About_Us_Controller::class, 'index']);
     Route::post('/home', [HotelController::class, 'AllHomeData']);
+
+    
 
 
     Route::post('/CreateNewsLetter', [NewsController::class, 'CreateNewsLetter']);
@@ -97,6 +169,7 @@ Route::middleware(['Api_Auth'])->group(function () {
     Route::get('/all-hotel-magazines', [HotelMagazinesController::class, 'AllHotelMagazines']);
   
 });
+
 
 Route::middleware(['user_login'])->group(function () {
     Route::post('/user-logout', [UserController::class, 'UserLogout']);
@@ -106,31 +179,28 @@ Route::middleware(['user_login'])->group(function () {
     Route::post('/sendOTP', [UserController::class, 'sendOTP']);
 
     Route::post('/add-distibutor', [DistibutionController::class, 'AddDistributor']);
-    Route::get('/all-distibutor', [DistibutionController::class, 'AllDistributor']);
-    Route::post('/edit-distibutor', [DistibutionController::class, 'EditDistributor']);
+   
     Route::post('/update-distibutor', [DistibutionController::class, 'UpdateDistributor']);
     Route::post('/delete-distibutor', [DistibutionController::class, 'DeleteDistributor']);
 
 
 
     Route::post('/add-distibutor-data', [DistibutionController::class, 'AddDistributorData']);
-    Route::get('/all-distibutor-data', [DistibutionController::class, 'AllDistributorData']);
-    Route::post('/edit-distibutor-data', [DistibutionController::class, 'EditDistributorData']);
+   
     Route::post('/update-distibutor-data', [DistibutionController::class, 'UpdateDistributorData']);
     Route::post('/delete-distibutor-data', [DistibutionController::class, 'DeleteDistributorData']);
 
     
     Route::post('/add-distibutor-detail', [DistibutionController::class, 'AddDistributorDetail']);
-    Route::get('/all-distibutor-detail', [DistibutionController::class, 'AllDistributorDetail']);
-    Route::post('/edit-distibutor-detail', [DistibutionController::class, 'EditDistributorDetail']);
+    
     Route::post('/update-distibutor-detail', [DistibutionController::class, 'UpdateDistributorDetail']);
     Route::post('/delete-distibutor-detail', [DistibutionController::class, 'DeleteDistributorDetail']);
     
-    Route::get('/all-news/{id?}', [NewsController::class, 'index']);
+ 
 	Route::post('/create-news', [NewsController::class, 'CreateNews']);
-    Route::post('/edit-news', [NewsController::class, 'EditNews']);
+  
     Route::post('/update-news', [NewsController::class, 'UpdateNews']);
-    Route::post('/views-news', [NewsController::class, 'ViewNews']);
+
     Route::post('/delete-news', [NewsController::class, 'DeleteNews']);
     Route::post('/delete_single_news_image', [NewsController::class, 'delete_single_news_image']);
     Route::post('/update_single_news_image', [NewsController::class, 'update_single_news_image']);
@@ -139,19 +209,18 @@ Route::middleware(['user_login'])->group(function () {
     // news category
     
     Route::post('/add_news_category', [NewsController::class, 'add_news_category']);
-    Route::post('/edit_news_category', [NewsController::class, 'edit_news_category']);
+    
     Route::post('/update_news_category', [NewsController::class, 'update_news_category']);
-    Route::get('/all_news_category', [NewsController::class, 'all_news_category']);
+    
     Route::post('/delete_news_category', [NewsController::class, 'delete_news_category']);
     
 
     
     Route::post('/add-home-hotel', [HomeHotelController::class, 'AddHomeHotel']);
-    Route::get('/all-home-hotel', [HomeHotelController::class, 'AllHomeHotel']);
-    Route::post('/edit-home-hotel', [HomeHotelController::class, 'EditHomeHotel']);
+  
     Route::post('/update-home-hotel', [HomeHotelController::class, 'UpdateHomeHotel']);
     Route::post('/delete-home-hotel', [HomeHotelController::class, 'DeleteHomeHotel']);
-    Route::post('/limit-all-home-hotel', [HomeHotelController::class, 'LimitAllHomeHotel']);
+
 
     
    
@@ -160,7 +229,7 @@ Route::middleware(['user_login'])->group(function () {
     Route::post('/payment', [PaymentController::class, 'payment']);
      Route::post('/add-hotel-magazines', [HotelMagazinesController::class, 'AddHotelMagazines']);
    
-     Route::post('/edit-hotel-magazines', [HotelMagazinesController::class, 'EditHotelMagazines']);
+   
      Route::post('/update-hotel-magazines', [HotelMagazinesController::class, 'UpdateHotelMagazines']);
      Route::post('/delete-hotel-magazines', [HotelMagazinesController::class, 'DeleteHotelMagazines']);
      Route::post('/delete-hotel-magazines-single-pdffile', [HotelMagazinesController::class, 'DeleteHotelMagazinesSinglePdffile']);
@@ -169,8 +238,8 @@ Route::middleware(['user_login'])->group(function () {
 
      
          Route::post('/hotel-register', [HotelController::class, 'HotelRegister']);
-    Route::get('/all-hotels', [HotelController::class, 'AllHotels']);
-    Route::post('/edit-hotels', [HotelController::class, 'EditHotels']);
+ 
+
     Route::post('/update-hotels', [HotelController::class, 'UpdateHotels']);
     Route::post('/delete-hotels', [HotelController::class, 'DeleteHotels']);
 
@@ -187,16 +256,14 @@ Route::middleware(['user_login'])->group(function () {
     Route::post('/add-hotel-ameties', [HotelController::class, 'AddHotelAmeties']);
     Route::post('/update-hotel-ameties', [HotelController::class, 'UpdateHotelAmeties']);
     Route::post('/delete-hotel-ameties', [HotelController::class, 'DeleteHotelAmeties']);
-    Route::get('/all-hotel-ameties', [HotelController::class, 'AllHotelAmeties']);
-    Route::post('/edit-hotel-ameties', [HotelController::class, 'EditHotelAmeties']);
+  
 
     Route::post('/sort_order_ameties', [HotelController::class, 'sort_order_ameties']);
 
     Route::post('/add-hotel-facilities', [HotelFacilitiesController::class, 'AddHotelFacilities']);
     Route::post('/update-hotel-facilities', [HotelFacilitiesController::class, 'UpdateHotelFacilities']);
     Route::post('/delete-hotel-facilities', [HotelFacilitiesController::class, 'DeleteHotelFacilities']);
-    Route::get('/all-hotel-facilities', [HotelFacilitiesController::class, 'AllHotelFacilities']);
-    Route::post('/edit-hotel-facilities', [HotelFacilitiesController::class, 'EditHotelFacilities']);
+   
 
     Route::post('/sort_order_facilities', [HotelFacilitiesController::class, 'sort_order_facilities']);
 
@@ -204,46 +271,41 @@ Route::middleware(['user_login'])->group(function () {
 
 
     Route::post('/add-media-kit', [MediakitController::class, 'AddMediaKit']);
-    Route::get('/all-media-kit', [MediakitController::class, 'AllMediaKit']);
     Route::post('/update-media-kit', [MediakitController::class, 'UpdateMediaKit']);
     Route::post('/delete-media-kit', [MediakitController::class, 'DeleteMediaKit']);
-    Route::post('/edit-media-kit', [MediakitController::class, 'EditMediaKit']);
+  
 
 
     Route::post('/add-banner', [BannerController::class, 'AddBanner']);
-    Route::get('/all-banner', [BannerController::class, 'AllBanner']);
+
     Route::post('/update-banner', [BannerController::class, 'UpdateBanner']);
     Route::post('/delete-banner', [BannerController::class, 'DeleteBanner']);
-    Route::post('/edit-banner', [BannerController::class, 'EditBanner']);
-
+  
     Route::post('/add-query', [QueriesController::class, 'AddQuery']);
-    Route::get('/all-query', [QueriesController::class, 'AllQuery']);
+   
     Route::post('/update-query', [QueriesController::class, 'UpdateQuery']);
     Route::post('/delete-query', [QueriesController::class, 'DeleteQuery']);
-    Route::post('/edit-query', [QueriesController::class, 'EditQuery']);
+  
 
     Route::post('/send_query_reply', [QueriesController::class, 'send_query_reply']);
     
 
     Route::post('/add-team', [TeamController::class, 'AddTeam']);
-    Route::get('/all-team', [TeamController::class, 'AllTeam']);
+  
     Route::post('/update-team', [TeamController::class, 'UpdateTeam']);
     Route::post('/delete-team', [TeamController::class, 'DeleteTeam']);
-    Route::post('/edit-team', [TeamController::class, 'EditTeam']);
-
+   
 
     Route::post('/add-voted-hotel', [VotedHotelController::class, 'AddVotedHotel']);
     Route::post('/update-voted-hotel', [VotedHotelController::class, 'UpdateVotedHotel']);
     Route::post('/delete-voted-hotel', [VotedHotelController::class, 'DeleteVotedHotel']);
-    Route::get('/all-voted-hotel', [VotedHotelController::class, 'AllVotedHotel']);
-    Route::post('/edit-voted-hotel', [VotedHotelController::class, 'EditVotedHotel']);
+   
 
 
     Route::post('/add-hotel-properties', [PropertiesController::class, 'AddHotelProperties']);
     Route::post('/update-hotel-properties', [PropertiesController::class, 'UpdateHotelProperties']);
     Route::post('/delete-hotel-properties', [PropertiesController::class, 'DeleteHotelProperties']);
-    Route::get('/all-hotel-properties', [PropertiesController::class, 'AllHotelProperties']);
-    Route::post('/edit-hotel-properties', [PropertiesController::class, 'EditHotelProperties']);
+    
 
 
     Route::post('/add-package-title', [PackagePriceController::class, 'AddPackageTitle']);
@@ -251,14 +313,12 @@ Route::middleware(['user_login'])->group(function () {
     Route::post('/add-package-price', [PackagePriceController::class, 'AddPackagePrice']);
     Route::post('/update-package-price', [PackagePriceController::class, 'UpdatePackagePrice']);
     Route::post('/delete-package-price', [PackagePriceController::class, 'DeletePackagePrice']);
-    Route::get('/all-package-price', [PackagePriceController::class, 'AllPackagePrice']);
-    Route::post('/edit-package-price', [PackagePriceController::class, 'EditPackagePrice']);
+   
 
     
 
     Route::post('/add-subscriber', [SubscribersController::class, 'AddSubscriber']);
-    Route::get('/all-subscriber', [SubscribersController::class, 'AllSubscriber']);
-    Route::post('/edit-subscriber', [SubscribersController::class, 'EditSubscriber']);
+    
     Route::post('/update-subscriber', [SubscribersController::class, 'UpdateSubscriber']);
     Route::post('/delete-subscriber', [SubscribersController::class, 'DeleteSubscriber']);
 
@@ -267,13 +327,12 @@ Route::middleware(['user_login'])->group(function () {
     // Route::post('/login-user-hotel-ameties', [HotelController::class, 'LoginUserHotelAmeties']);
 
     Route::post('/create-review', [ReviewController::class, 'CreateReview']);
-    Route::get('/all-review/{type?}', [ReviewController::class, 'index']);
+
     Route::post('/delete-review', [ReviewController::class, 'DeleteReview']);
 
 
     Route::post('/update-about-us', [About_Us_Controller::class, 'update_about_us']);
-    Route::get('/edit-about-us', [About_Us_Controller::class, 'index']);
-    Route::post('/edit-magzine-distributed', [About_Us_Controller::class, 'edit_magzine_distributed']);
+   
     Route::post('/update-magzine-distributed', [About_Us_Controller::class, 'update_magzine_distributed']);
     
     Route::post('/stripe-Single-Payment', [PaymentController::class, 'stripe_Single_Payment']);
@@ -283,9 +342,9 @@ Route::middleware(['user_login'])->group(function () {
 
     
     Route::post('/create-review-category', [HotelController::class, 'Create_Review_Category']);
-    Route::get('/all-review-categories', [HotelController::class, 'All_Review_Categories']);
+  
     Route::post('/create-review-topic', [HotelController::class, 'Create_Review_Topic']);
-    Route::get('/all-review-topics', [HotelController::class, 'All_Review_Topics']);
+   
     Route::post('/create-guest-review', [HotelController::class, 'Create_Guest_Review']);
     Route::post('/get-reviews-by-topics', [HotelController::class, 'Get_Reviews_By_Topics']);
 
@@ -295,13 +354,13 @@ Route::middleware(['user_login'])->group(function () {
 Route::post('/add_home_info', [HomeInfoController::class, 'add_home_info']);
 Route::post('/update_home_info', [HomeInfoController::class, 'update_home_info']);
 Route::post('/delete_home_info', [HomeInfoController::class, 'delete_home_info']);
-Route::post('/edit_home_info', [HomeInfoController::class, 'edit_home_info']);
+
 
 Route::post('/single_page_details', [HomeInfoController::class, 'single_page_details']);
 Route::post('/update_single_home_info', [HomeInfoController::class, 'update_single_home_info']);
-Route::post('/get_single_page_details', [HomeInfoController::class, 'get_single_page_details']);
+
 Route::post('/delete_magazine_cover', [HomeInfoController::class, 'delete_magazine_cover']);
-Route::post('/edit_magazine_cover', [HomeInfoController::class, 'edit_magazine_cover']);
+
 
 // advertise with us 
 
